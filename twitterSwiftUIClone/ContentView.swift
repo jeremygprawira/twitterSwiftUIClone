a7c4b9c5cd09d308db6618f672b406456c295634
Jeremy <jeremygprawira@gmail.com>
2022-06-27 19:22:27 +0700

    Initial Commit

---
+++ twitterSwiftUIClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  twitterSwiftUIClone
+//
+//  Created by Jeremy Gerald Prawira on 27/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ twitterSwiftUIClone/twitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterSwiftUICloneApp.swift
+//  twitterSwiftUIClone
+//
+//  Created by Jeremy Gerald Prawira on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct twitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
