7485b5a30c8b4d6a1fb4960b7a862e5de65f4be2
Jeremy <jeremygprawira@gmail.com>
2022-07-01 17:07:40 +0700

    Continue on SideMenu

--- twitterSwiftUIClone/twitterSwiftUICloneApp.swift
+++ twitterSwiftUIClone/twitterSwiftUICloneApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct twitterSwiftUICloneApp: App {
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
